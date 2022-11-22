22c3d9386c4d53d059752a069034d8351b51090b
pythonbrad <fomegnemeudje@outlook.com>
2022-11-22 23:50:05 +0100

    refactor: rename the project (#6)

--- foody/asgi.py
+++ foody/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for finefood project.
+ASGI config for foody project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'finefood.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'foody.settings')
 
 application = get_asgi_application()
