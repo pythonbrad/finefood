0c129fbcd84d3693efe3420456652f8e8aa30651
Fomegne <fomegnemeudje@outlook.com>
2023-01-23 09:57:47 +0100

    fix: urls

--- core/urls.py
+++ core/urls.py
@@ -5,9 +5,9 @@ app_name = 'core'
 
 urlpatterns = [
     path('', views.index, name='home'),
-    path('search', views.restaurants_list, name='restaurants_list'),
-    path('accounts/login', views.login, name='login'),
-    path('accounts/signin', views.signin, name='signin'),
-    path('accounts/logout', views.logout, name='logout'),
+    path('search/', views.restaurants_list, name='restaurants_list'),
+    path('accounts/login/', views.login, name='login'),
+    path('accounts/signin/', views.signin, name='signin'),
+    path('accounts/logout/', views.logout, name='logout'),
     path('<code>', views.restaurant_page, name='restaurant_page'),
 ]
